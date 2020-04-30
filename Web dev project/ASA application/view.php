8d8bbc0ac8c6a92d27e14b0423d928aa178eaf30
Wendy123-4 <55187065+Wendy123-4@users.noreply.github.com>
2020-04-30 18:45:50 +0000

    Add files via upload

---
+++ Web dev project/ASA application/view.php
@@ -0,0 +1,72 @@
+<?php
+
+if(isset($_POST['search']))
+{
+    $valueToSearch = $_POST['valueToSearch'];
+    // search in all table columns
+    // using concat mysql function
+    $query = "SELECT * FROM `form` WHERE CONCAT(`candidate_name`, `contact`) LIKE '%".$valueToSearch."%'";
+    $search_result = filterTable($query);
+
+}
+ else {
+    $query = "SELECT * FROM `form`";
+    $search_result = filterTable($query);
+}
+
+// function to connect and execute the query
+function filterTable($query)
+{
+    $connect = mysqli_connect("localhost", "root", "", "application_form");
+    $filter_Result = mysqli_query($connect, $query);
+    return $filter_Result;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP HTML TABLE DATA SEARCH</title>
+        <style>
+            table,tr,th,td
+            {
+                border: 1px solid black;
+            }
+        </style>
+    </head>
+    <body>
+
+        <form action="view.php" method="post">
+            <input type="text" name="valueToSearch" placeholder="Value To Search"><br><br>
+            <input type="submit" name="search" value="Filter"><br><br><br><br>
+
+            <table style="text-align:center">
+                <tr>
+                    <th>Candidate</th>
+                    <th>Email</th>
+                    <th>Contact</th>
+                    <th>Address</th>
+                    <th>About Yourself</th>
+                    <th>Interests</th>
+                    <th>Why ASA</th>
+                </tr>
+
+      <!-- populate table from mysql database -->
+                <?php while($row = mysqli_fetch_array($search_result)):?>
+                <tr>
+                    <td><?php echo $row['candidate_name'];?></td>
+                    <td><?php echo $row['candidate_email'];?></td>
+                    <td><?php echo $row['contact'];?></td>
+                    <td><?php echo $row['address'];?></td>
+                    <td><?php echo $row['about_yourself'];?></td>
+                    <td><?php echo $row['interests'];?></td>
+                    <td><?php echo $row['why_asa'];?></td>
+                </tr>
+                <?php endwhile;?>
+            </table>
+        </form>
+
+    </body>
+</html>
+
