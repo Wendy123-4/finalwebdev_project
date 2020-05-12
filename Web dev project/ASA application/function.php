900597d2aa55fd9563855990ee694a5d8f96adc5
Wendy123-4 <55187065+Wendy123-4@users.noreply.github.com>
2020-05-12 08:50:55 +0000

    Update function.php

--- Web dev project/ASA application/function.php
+++ Web dev project/ASA application/function.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
 session_start();
 
 // connect to database
 $db = mysqli_connect('localhost', 'root', '', 'multi_login');
-$db2 = mysqli_connect('localhost', 'root', '', 'application_form');
 
 // variable declaration
 $username = "";
@@ -18,6 +17,7 @@ $contact = "";
 $address = "";
 $interests = "";
 $why_asa = "";
+
 
 
 // call the register() function if register_btn is clicked
@@ -38,14 +38,14 @@ function register(){
 	$password_2  =  e($_POST['password_2']);
 
 	// form validation: ensure that the form is correctly filled
-	if (empty($username)) { 
-		array_push($errors, "Username is required"); 
+	if (empty($username)) {
+		array_push($errors, "Username is required");
 	}
-	if (empty($email)) { 
-		array_push($errors, "Email is required"); 
+	if (empty($email)) {
+		array_push($errors, "Email is required");
 	}
-	if (empty($password_1)) { 
-		array_push($errors, "Password is required"); 
+	if (empty($password_1)) {
+		array_push($errors, "Password is required");
 	}
 	if ($password_1 != $password_2) {
 		array_push($errors, "The two passwords do not match");
@@ -57,13 +57,13 @@ function register(){
 
 		if (isset($_POST['user_type'])) {
 			$user_type = e($_POST['user_type']);
-			$query = "INSERT INTO users (username, email, user_type, password) 
+			$query = "INSERT INTO users (username, email, user_type, password)
 					  VALUES('$username', '$email', '$user_type', '$password')";
 			mysqli_query($db, $query);
 			$_SESSION['success']  = "New user successfully created!!";
 			header('location: home.php');
 		}else{
-			$query = "INSERT INTO users (username, email, user_type, password) 
+			$query = "INSERT INTO users (username, email, user_type, password)
 					  VALUES('$username', '$email', 'user', '$password')";
 			mysqli_query($db, $query);
 
@@ -72,7 +72,7 @@ function register(){
 
 			$_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
 			$_SESSION['success']  = "You are now logged in";
-			header('location: index.php');				
+			header('location: index.php');
 		}
 	}
 }
@@ -103,7 +103,7 @@ function display_error() {
 			}
 		echo '</div>';
 	}
-}	
+}
 
 function isLoggedIn()
 {
@@ -113,12 +113,17 @@ function isLoggedIn()
 		return false;
 	}
 }
+
+
+// log user out if logout button clicked
 if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION['user']);
 	header("location: login.php");
 }
 
+
+// call the login() function if register_btn is clicked
 if (isset($_POST['login_btn'])) {
 	login();
 }
@@ -153,7 +158,7 @@ function login(){
 
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
-				header('location: home.php');
+				header('location: admin/home.php');
 			}else{
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
@@ -174,7 +179,6 @@ function isAdmin()
 		return false;
 	}
 }
-
 
 
 // db2
